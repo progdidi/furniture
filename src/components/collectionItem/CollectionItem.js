280b15d582696b54bbcaab1274c25b510a7c3fe8
Diana Sosnovskaya <90276229+progdidi@users.noreply.github.com>
2024-06-10 09:26:03 +0000

    feat: add collectionItem component, collections section

---
+++ src/components/collectionItem/CollectionItem.js
@@ -0,0 +1,17 @@
+import "./collectionItem.scss";
+
+const CollectionItem = ({ img, name, link }) => {
+  return (
+    <div className="collection-item">
+      <img src={img} alt="" className="collection-item__img" />
+      <div className="collection-item__info">
+        <h5 className="collection-item__title">{name}</h5>
+        <a href="" className="collection-item__link">
+          Shop Now
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default CollectionItem;
---
+++ src/helpers/collections/collections.js
@@ -0,0 +1,25 @@
+import col1 from "./images/col1.png";
+import col2 from "./images/col2.png";
+import col3 from "./images/col3.png";
+
+const collections = [
+  {
+    name: "Living Room",
+    img: col1,
+    id: 1,
+  },
+
+  {
+    name: "Bedroom",
+    img: col2,
+    id: 2,
+  },
+
+  {
+    name: "Kitchen",
+    img: col3,
+    id: 3,
+  },
+];
+
+export { collections };
