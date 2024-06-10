e7695ceca3122179e5ecb8557ee208e048575624
Diana Sosnovskaya <progdidi@gmail.com>
2024-06-11 00:42:32 +0300

    feat: add productPage

--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -3,13 +3,32 @@ import './productPage.scss';
 //components
 import ReviewItem from './reviewItem/ReviewItem';
 import { reviews } from '../../helpers/reviews/reviews';
+import Newsletter from '../../components/newsletter/Newsletter';
+
+//images
+import example from './example.png';
 
 const ProductPage = () => {
     return ( 
         <>
 
+        <section className="product">
+            <div className="container">
+                <div className="product__inner">
+                    <div className="product-media">
+                        <img src={example} alt="" className="product-media__img" />
+                    </div>
+                    <div className="product-info">
+                        <h4 className="product-info__title">Tray Table</h4>
+                        <div className="product-info__descr">Buy one or buy a few and make every space where you sit more convenient. Light and easy to move around with removable tray top, handy for serving snacks.</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
         <section className="reviews">
             <div className="container">
+                <h4 className="reviews__title section__title">Customer Reviews</h4>
                 {reviews.map((review) => {
                 return (
                     <ReviewItem
@@ -23,6 +42,8 @@ const ProductPage = () => {
                 })}
             </div>
         </section>
+
+        <Newsletter/>
         </>
      );
 }
