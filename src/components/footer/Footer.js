2315e597755018698451472f57dc552aae5bc650
Diana Sosnovskaya <90276229+progdidi@users.noreply.github.com>
2024-06-10 14:10:41 +0000

    fix: fix footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,62 @@
+import "./footer.scss";
+
 const Footer = () => {
-    return ( 
-        <footer className="footer">
-            
-        </footer>
-     );
-}
- 
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-top">
+          <div className="footer-top__title">
+            <a href="" className="footer-top__logo">
+              Furniture
+            </a>
+            <p className="footer-top__text">Gift & Decoration Store</p>
+          </div>
+          <nav className="footer-menu">
+            <ul className="footer-menu__list">
+              <li className="footer-menu__list-item">
+                <a href="" className="footer-menu__list-link">
+                  Home
+                </a>
+              </li>
+              <li className="footer-menu__list-item">
+                <a href="" className="footer-menu__list-link">
+                  Shop
+                </a>
+              </li>
+              <li className="footer-menu__list-item">
+                <a href="" className="footer-menu__list-link">
+                  Product
+                </a>
+              </li>
+              <li className="footer-menu__list-item">
+                <a href="" className="footer-menu__list-link">
+                  Blog
+                </a>
+              </li>
+              <li className="footer-menu__list-item">
+                <a href="" className="footer-menu__list-link">
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="footer-bottom">
+          <p className="footer-bottom__text">
+            Copyright © 2023 3legant. All rights reserved
+          </p>
+          <div className="footer-bottom__links">
+            <a href="" className="footer-bottom__link">
+              Privacy Policy
+            </a>
+            <a href="" className="footer-bottom__link">
+              Terms of Use
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
