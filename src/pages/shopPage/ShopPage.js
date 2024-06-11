f243d0647e369626ddb59aafdcf3f7a483d24598
Diana Sosnovskaya <90276229+progdidi@users.noreply.github.com>
2024-06-11 14:27:22 +0000

    fix: fix shoppage

--- src/pages/shopPage/ShopPage.js
+++ src/pages/shopPage/ShopPage.js
@@ -8,6 +8,42 @@ const ShopPage = () => {
         title="Shop Page"
         descr="Let’s design the place you always imagined."
       />
+
+      <section className="shop">
+        <div className="container">
+          <div className="shop__inner">
+            <div className="shop-filter">
+              <ul className="filter-list">
+                <li className="filter-list__item">
+                  <button className="filter-list-btn">Living Room</button>
+                </li>
+                <li className="filter-list__item">
+                  <button className="filter-list-btn">Bedroom</button>
+                </li>
+                <li className="filter-list__item">
+                  <button className="filter-list-btn">Kitchen</button>
+                </li>
+                <li className="filter-list__item">
+                  <button className="filter-list-btn">Bathroom</button>
+                </li>
+                <li className="filter-list__item">
+                  <button className="filter-list-btn">Dinning</button>
+                </li>
+                <li className="filter-list__item">
+                  <button className="filter-list-btn">Outdoor</button>
+                </li>
+                <li className="filter-list__item">
+                  <button className="filter-list-btn">Outdoor</button>
+                </li>
+                <li className="filter-list__item">
+                  <button className="filter-list-btn">Outdoor</button>
+                </li>
+              </ul>
+            </div>
+            <div className="shop__items"></div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
