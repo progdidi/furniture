9b6f5aa288851300ea52bfd1662acc747fb8dad7
Diana Sosnovskaya <90276229+progdidi@users.noreply.github.com>
2024-06-11 12:25:28 +0000

    fix: fix timer component styles

--- src/pages/ProductPage/offerTimer/OfferTimer.js
+++ src/pages/ProductPage/offerTimer/OfferTimer.js
@@ -6,20 +6,20 @@ const OfferTimer = () => {
       <p className="product-offer__text">Offer expires in:</p>
       <div className="product-offer__timer">
         <div className="product-offer__timer-item">
-          <p className="product-offer__timer-num">00</p>
-          <p className="product-offer__timer-text"></p>
+          <p className="product-offer__timer-num">02</p>
+          <p className="product-offer__timer-text">Days</p>
         </div>
         <div className="product-offer__timer-item">
-          <p className="product-offer__timer-num"></p>
-          <p className="product-offer__timer-text"></p>
+          <p className="product-offer__timer-num">12</p>
+          <p className="product-offer__timer-text">Hours</p>
         </div>
         <div className="product-offer__timer-item">
-          <p className="product-offer__timer-num"></p>
-          <p className="product-offer__timer-text"></p>
+          <p className="product-offer__timer-num">30</p>
+          <p className="product-offer__timer-text">Minutes</p>
         </div>
         <div className="product-offer__timer-item">
-          <p className="product-offer__timer-num"></p>
-          <p className="product-offer__timer-text"></p>
+          <p className="product-offer__timer-num">10</p>
+          <p className="product-offer__timer-text">Seconds</p>
         </div>
       </div>
     </div>
