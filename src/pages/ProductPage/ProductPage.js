70df2d161273bca34df55a7b638fb675d0799998
Diana Sosnovskaya <90276229+progdidi@users.noreply.github.com>
2024-06-11 14:11:53 +0000

    fix: fix productpage component

--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -5,6 +5,7 @@ import ReviewItem from "./reviewItem/ReviewItem";
 import { reviews } from "../../helpers/reviews/reviews";
 import Newsletter from "../../components/newsletter/Newsletter";
 import OfferTimer from "./offerTimer/OfferTimer";
+import ProductRating from "../../components/productRating/ProductRating";
 
 //images
 import example from "./example.png";
@@ -101,17 +102,30 @@ const ProductPage = () => {
       <section className="reviews">
         <div className="container">
           <h4 className="reviews__title section__title">Customer Reviews</h4>
-          {reviews.map((review) => {
-            return (
-              <ReviewItem
-                img={review.img}
-                name={review.name}
-                text={review.text}
-                rating={review.rating}
-                key={review.id}
-              />
-            );
-          })}
+          <ProductRating />
+
+          <form action="" className="review-form">
+            <input type="text" className="review-form__input" />
+            <button className="review-form__btn main-btn">Write Review</button>
+          </form>
+
+          <div className="reviews__items">
+            <h5 className="reviews__items-title">11 Reviews</h5>
+
+            {reviews.map((review) => {
+              return (
+                <ReviewItem
+                  img={review.img}
+                  name={review.name}
+                  text={review.text}
+                  rating={review.rating}
+                  key={review.id}
+                />
+              );
+            })}
+          </div>
+
+          <button className="reviews-btn">Load more</button>
         </div>
       </section>
 
