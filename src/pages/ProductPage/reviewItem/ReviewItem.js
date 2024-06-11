f53795996d704ff4e384d774b09291c1d410b509
Diana Sosnovskaya <90276229+progdidi@users.noreply.github.com>
2024-06-11 13:50:33 +0000

    fix reviewItem styles

--- src/pages/ProductPage/reviewItem/ReviewItem.js
+++ src/pages/ProductPage/reviewItem/ReviewItem.js
@@ -1,16 +1,20 @@
-import './reviewItem.scss';
+import "./reviewItem.scss";
 
-const ReviewItem = ({img, name, text, rating}) => {
-    return ( 
-        <div className="review-item">
-            <img src={img} alt="" className="review-item__img" />
-            <div className="review-item__info">
-                <h6 className="review-item__info-title">{name}</h6>
-                <div className="review-item__info-rating">{rating}</div>
-                <p className="review-item__info-text">{text}</p>
-            </div>
+const ReviewItem = ({ img, name, text, rating }) => {
+  return (
+    <div className="review-item">
+      <img src={img} alt="" className="review-item__img" />
+      <div className="review-item__info">
+        <h6 className="review-item__info-title">{name}</h6>
+        <div className="review-item__info-rating">{rating}</div>
+        <p className="review-item__info-text">{text}</p>
+        <div className="review-item__buttons">
+          <button className="review-item__btn">like</button>
+          <button className="review-item__btn">replay</button>
         </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default ReviewItem;
