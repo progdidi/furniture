582168ca343271c85b9de8733630fc6b80388cc1
Diana Sosnovskaya <progdidi@gmail.com>
2024-06-13 14:03:20 +0300

    fix: fix produtPage

--- src/pages/shopPage/ShopPage.js
+++ src/pages/shopPage/ShopPage.js
@@ -1,6 +1,17 @@
 import "./shopPage.scss";
+
+//images
+import view1 from './view1.svg';
+import view2 from './view2.svg';
+import view3 from './view3.svg';
+import view4 from './view4.svg';
+
+//components
 import PagePreview from "../../components/pagePreview/PagePreview";
 import ProductFilter from "./productFilter/ProductFilter";
+
+import ProductCard from "../../components/productCard/ProductCard";
+import { products } from "../../helpers/products/products";
 
 const ShopPage = () => {
   return (
@@ -14,7 +25,51 @@ const ShopPage = () => {
         <div className="container">
           <div className="shop__inner">
             <ProductFilter/>
-            <div className="shop__items"></div>
+            <div className="shop-main">
+
+              <div className="shop-main__header">
+                <h4 className="shop-main__title">Living Room</h4>
+
+                <label className="product-sort__label"> Sort by
+                  <select className="product-sort">
+                    <option value="" className="product-sort__item">1</option>
+                    <option value="" className="product-sort__item">2</option>
+                    <option value="" className="product-sort__item">3</option>
+                  </select>
+                </label>
+                
+
+                <div className="shop-main__view">
+                  <button className="shop-main__view-btn">
+                    <img src={view1} alt="" className="shop-main__view-btn-img" />
+                  </button>
+                  <button className="shop-main__view-btn active">
+                    <img src={view2} alt="" className="shop-main__view-btn-img" />
+                  </button>
+                  <button className="shop-main__view-btn">
+                    <img src={view3} alt="" className="shop-main__view-btn-img" />
+                  </button>
+                  <button className="shop-main__view-btn">
+                    <img src={view4} alt="" className="shop-main__view-btn-img" />
+                  </button></div>
+              </div>
+
+              <div className="product-items">
+                {products.map((product) => {
+                  return (
+                    <ProductCard
+                      name={product.name}
+                      price={product.price}
+                      key={product.id}
+                    />
+                  );
+                })}
+              </div>
+
+              <button className="shop-main__btn">Show more</button>
+
+              
+            </div>
           </div>
         </div>
       </section>
