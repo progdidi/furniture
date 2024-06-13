55a30c3d6a6d61685e1a668fcb4530a07a8f954b
Diana Sosnovskaya <progdidi@gmail.com>
2024-06-14 00:53:42 +0300

    fix: fix media styles mainpage

--- src/components/articlesItem/ArticlesItem.js
+++ src/components/articlesItem/ArticlesItem.js
@@ -2,14 +2,14 @@ import "./articlesItem.scss";
 
 const ArticlesItem = ({ img, title, date }) => {
   return (
-    <div className="articles-item">
+    <a href="" className="articles-item">
       <img src={img} alt="" className="articles-item__img" />
       <h5 className="articles-item__title">{title}</h5>
       <a href="" className="articles-item__link main-link">
         Read More
       </a>
       <p className="articles-item__date">{date}</p>
-    </div>
+    </a>
   );
 };
 
