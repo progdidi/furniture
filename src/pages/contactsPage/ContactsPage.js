0b9099312d33d6433b280f5e13a1ca6db133010a
Diana Sosnovskaya <progdidi@gmail.com>
2024-06-15 15:09:10 +0300

    fix: fix contactsPage

--- src/pages/contactsPage/ContactsPage.js
+++ src/pages/contactsPage/ContactsPage.js
@@ -14,18 +14,16 @@ const ContactsPage = () => {
         <>
             <section className="about">
                 <div className="container">
-                    <div className="about__inner">
-                        <h4 className="about__title page__title">We believe in sustainable decor. We’re passionate about life at home.</h4>
-                        <p className="about__text">Our features timeless furniture, with natural fabrics, curved lines, plenty of mirrors and classic design, which can be incorporated into any decor project. The pieces enchant for their sobriety, to last for generations, faithful to the shapes of each period, with a touch of the present</p>
+                    <h4 className="about__title page__title">We believe in sustainable decor. We’re passionate about life at home.</h4>
+                    <p className="about__text">Our features timeless furniture, with natural fabrics, curved lines, plenty of mirrors and classic design, which can be incorporated into any decor project. The pieces enchant for their sobriety, to last for generations, faithful to the shapes of each period, with a touch of the present</p>
 
-                        <div className="about-main">
-                            <img src={contacts} alt="" className="about-main__img" />
-                            <div className="about-main__info">
-                                <h5 className="about-main__info-title section__title">About Us</h5>
-                                <p className="about-main__info-text">3legant is a gift & decorations store based in HCMC, Vietnam. Est since 2019. 
+                    <div className="about-main">
+                        <img src={contacts} alt="" className="about-main__img" />
+                        <div className="about-main__info">
+                            <h5 className="about-main__info-title section__title">About Us</h5>
+                            <p className="about-main__info-text">3legant is a gift & decorations store based in HCMC, Vietnam. Est since 2019. 
 Our customer service is always prepared to support you 24/7</p>
-                                <a href="" className="about-main__info-link main-link">Shop Now</a>
-                            </div>
+                            <a href="" className="about-main__info-link main-link">Shop Now</a>
                         </div>
                     </div>
                 </div>
@@ -54,26 +52,30 @@ Our customer service is always prepared to support you 24/7</p>
 
                     <div className="contacts__inner">
                         <form action="" className="contacts-form">
-                            <label htmlFor="" className="contacts-form__label">
+                            <label htmlFor="" className="contacts-form__label">Full Name
                                 <input type="text" className="contacts-form__input"  placeholder='Your Name'/>
                             </label>
-                            <label htmlFor="" className="contacts-form__label">
+                            <label htmlFor="" className="contacts-form__label">Email address
                                 <input type="text" className="contacts-form__input"  placeholder='Your Email'/>
                             </label>
-                            <label htmlFor="" className="contacts-form__label">
+                            <label htmlFor="" className="contacts-form__label">message
                                 <textarea name="" id="" cols="30" rows="10" className="contacts-form__text" placeholder='Your message'></textarea>
                             </label>
                             <button className="contacts-form__btn main-btn">Send Message</button>
                         </form>
 
-                        <div className="contacts-map"></div>
+                        <div className="contacts-map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2144.3129997857013!2d60.20764897628221!3d57.49003555686636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43c1c3ff2c07259d%3A0x21e52644fd145934!2sGazeta%20%22Zvezda%22!5e0!3m2!1sen!2sby!4v1718452122947!5m2!1sen!2sby" width="600" height="440" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
                     </div>
 
                     
                 </div>
+
+                <Benefits/>
             </section>
 
-            <Benefits/>
+            
         </>
      );
 }
